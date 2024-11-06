db159ed0ec8c8179f4566fec4469d505dfc0887c
Ryota Kuzuya <git@example.com>
2024-11-06 12:42:50 +0900

    seichiesテーブルの外部キー制約を修正

--- src/database/migrations/20241019141411_create_seichies_table.sql
+++ src/database/migrations/20241019141411_create_seichies_table.sql
@@ -2,15 +2,18 @@
 -- +goose StatementBegin
 CREATE TABLE seichies (
     seichi_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    user_id INT UNSIGNED NOT NULL,
     seichi_name VARCHAR(255) NOT NULL,
     comment VARCHAR(255),
     latitude DECIMAL(10, 8) NOT NULL,
     longitude DECIMAL(11, 8) NOT NULL,
-    place_id INT NOT NULL REFERENCES places(place_id) ON DELETE CASCADE,
-    content_id INT NOT NULL REFERENCES contents(content_id) ON DELETE CASCADE,
+    place_id INT NOT NULL,
+    content_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES places(place_id) ON DELETE CASCADE,
+    FOREIGN KEY (content_id) REFERENCES contents(content_id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
