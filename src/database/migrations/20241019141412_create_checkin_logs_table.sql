c594a028aa29231dfa38e0091a3a5edcd91b2157
Ryota Kuzuya <git@example.com>
2024-11-06 12:45:42 +0900

    checkin_logsテーブルの外部キー制約を修正

--- src/database/migrations/20241019141412_create_checkin_logs_table.sql
+++ src/database/migrations/20241019141412_create_checkin_logs_table.sql
@@ -2,9 +2,10 @@
 -- +goose StatementBegin
 CREATE TABLE checkin_logs (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    user_id INT UNSIGNED,
     seichi_id INT NOT NULL REFERENCES seichies(seichi_id) ON DELETE CASCADE,
-    PRIMARY KEY (created_at, user_id)
+    PRIMARY KEY (created_at, user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE    
 );
 -- +goose StatementEnd
 
