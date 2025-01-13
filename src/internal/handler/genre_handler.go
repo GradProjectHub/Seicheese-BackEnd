4c53017a8d3ef23db09ab328fb4e63f845f8cf26
rkuzugit <rkuzuya@gmail.com>
2025-01-13 20:31:32 +0900

    ジャンル取得のログを追加

--- src/internal/handler/genre_handler.go
+++ src/internal/handler/genre_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 	"seicheese/models"
 
@@ -14,11 +15,14 @@ type GenreHandler struct {
 
 // ジャンル一覧取得API
 func (h *GenreHandler) GetGenres(c echo.Context) error {
+	log.Printf("GetGenres called")
 	genres, err := models.Genres().All(c.Request().Context(), h.DB)
 	if err != nil {
+		log.Printf("Error fetching genres: %v", err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "ジャンルの取得に失敗しました")
 	}
 
+	log.Printf("Found %d genres", len(genres))
 	c.Response().Header().Set("Content-Type", "application/json; charset=utf-8")
 	return c.JSON(http.StatusOK, genres)
 }
