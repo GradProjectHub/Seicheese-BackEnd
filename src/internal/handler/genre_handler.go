2785b05e427bdc69a2b910aa72f3fa190596e0ef
rkuzugit <rkuzuya@gmail.com>
2025-01-13 21:02:08 +0900

    ログの追加

--- src/internal/handler/genre_handler.go
+++ src/internal/handler/genre_handler.go
@@ -16,6 +16,11 @@ type GenreHandler struct {
 // ジャンル一覧取得API
 func (h *GenreHandler) GetGenres(c echo.Context) error {
 	log.Printf("GetGenres called")
+	
+	// リクエストの詳細をログ出力
+	log.Printf("Request Headers: %+v", c.Request().Header)
+	log.Printf("Authorization Header: %s", c.Request().Header.Get("Authorization"))
+
 	genres, err := models.Genres().All(c.Request().Context(), h.DB)
 	if err != nil {
 		log.Printf("Error fetching genres: %v", err)
@@ -23,6 +28,10 @@ func (h *GenreHandler) GetGenres(c echo.Context) error {
 	}
 
 	log.Printf("Found %d genres", len(genres))
+	if len(genres) > 0 {
+		log.Printf("First genre: %+v", genres[0])
+	}
+
 	c.Response().Header().Set("Content-Type", "application/json; charset=utf-8")
 	return c.JSON(http.StatusOK, genres)
 }
