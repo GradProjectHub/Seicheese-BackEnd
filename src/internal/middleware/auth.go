7051affe40cd7afc4d529cab3d24dc161e230660
RyotaKuzuya <rkuzuya@gmail.com>
2025-01-25 19:10:16 +0900

    インポートの追加

--- src/internal/middleware/auth.go
+++ src/internal/middleware/auth.go
@@ -17,7 +17,7 @@ import (
 	"firebase.google.com/go/v4/auth"
 	"github.com/labstack/echo/v4"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
-	"github.com/volatiletech/null"
+	"github.com/volatiletech/null/v8"
 )
 
 type AuthMiddleware struct {
