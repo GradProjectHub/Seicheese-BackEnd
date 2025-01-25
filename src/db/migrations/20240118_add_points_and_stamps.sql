cdfb26301c22d215d1b74e35a2150c176982b004
RyotaKuzuya <rkuzuya@gmail.com>
2025-01-25 15:32:51 +0900

    マイグレーション削除

--- src/db/migrations/20240118_add_points_and_stamps.sql
+++
@@ -1,9 +0,0 @@
--- ユーザーテーブルの変更
-ALTER TABLE users
-ADD COLUMN points INT NOT NULL DEFAULT 0,
-ADD COLUMN stamps JSON;
-
--- チェックインログテーブルの変更
-ALTER TABLE checkin_logs
-ADD COLUMN points INT NOT NULL DEFAULT 0,
-ADD COLUMN stamp_id INT; 
